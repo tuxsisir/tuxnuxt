d8e59dd5656a971043a6003b2a2e25065dc290f6
Sisir Ghimire Chettri <samratsisir294@gmail.com>
2021-12-28 00:10:46 -0800

    Add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>My blog navigation bar here</div>
+    <Nuxt />
+    <div>footer</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial />
+  <div>
+    asdf
+  </div>
 </template>
 
 <script>
