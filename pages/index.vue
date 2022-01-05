488f8487a08f0acc785cae0c635fe14ba418649d
Sisir Ghimire Chettri <samratsisir294@gmail.com>
2022-01-04 18:20:23 -0800

    Update main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,64 @@
 <template>
-  <div style="border:1px solid black;">
-    <div class="p-3">
+  <div>
+    <div class="p-3 mb-3 text-justify">
+      <p class="font-mono subpixel-antialiased leading-relaxed">
+        Hi, Thank you for reaching out here.
+      </p>
+
+      <p class="mb-3 font-mono subpixel-antialiased leading-relaxed">
+        You can call me <span class="font-bold">Sisir</span>, I am Software Developer currently
+        based in Greater Vancouver, BC originally from Nepal. At present, I am going back to school to pursue degree in Data Analytics.
+      </p>
+      <p class="font-mono subpixel-antialiased leading-relaxed">
+        This is where I may put my jibber-jabber writings to references and see backs. I hope that the things here would be of help for you too.
+        If you want to know more about me, <nuxt-link class="text-blue-500" to="/">
+          click here
+        </nuxt-link>.
+      </p>
+      <!--
       <img
-        class="object-cover h-48 w-96 mx-auto rounded-lg shadow-md"
+        class="object-cover h-48 w-9/12 mx-auto rounded-lg shadow-md"
         src="https://tailwindcss.com/img/card-top.jpg"
         alt="Sunset in the mountains"
       >
+      -->
     </div>
-    <div>
-      #javascript, #django, #postgres, #database
-    </div>
-    <div v-for="(article, key) in allArticles" :key="key">
-      {{ article.title }}
+    <div class="px-3">
+      <div class="font-bold mb-0">
+        { Tags }
+      </div>
+      <div class="mb-3">
+        =====
+      </div>
+      <div class="mb-5 text-center">
+        <span
+          v-for="(count, tag, tagKey) in countedTags"
+          :key="tagKey"
+          class="text-xs inline-block py-1 px-2 rounded text-blue-500
+        last:mr-0 mr-1 mb-2"
+          :style="'font-size:' + (2 + 7 * count) + 'px'"
+        >
+          #{{ tag }}
+        </span>
+      </div>
+      <div class="font-bold mb-0">
+        { Self Notes }
+      </div>
+      <div class="mb-3">
+        =========
+      </div>
+      <div v-for="(article, key) in allArticles" :key="key">
+        <div class="flex">
+          <div class="w-32">
+            [ {{ formatDate(article.date) }} ]
+          </div>
+          <div class="hover:text-white hover:bg-blue-500 text-blue-500 font-bold">
+            <nuxt-link :to="'/notes/' + article.slug">
+              {{ article.title }}
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,13 +68,18 @@
 export default {
   name: 'IndexPage',
   async asyncData ({ $content, params }) {
-    const allArticles = await $content('articles').fetch()
-    return { allArticles }
+    const allArticles = await $content('notes').sortBy('date', 'desc').fetch()
+    const tags = await $content('notes').sortBy('date', 'desc').only(['tags']).fetch()
+    const merged = [].concat.apply([], tags.map(x => (x.tags)))
+    const countedTags = {}
+    for (const x in merged) {
+      countedTags[merged[x]] = merged.filter(tag => tag === merged[x]).length
+    }
+    return { allArticles, countedTags }
   },
   methods: {
     formatDate (date) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' }
-      return new Date(date).toLocaleDateString('en', options)
+      return new Date(date).toISOString().slice(0, 10)
     }
   }
 }
