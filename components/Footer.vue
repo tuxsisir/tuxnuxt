3736c27b5742a4eaf48577cd45bb3dc934589e41
Sisir Ghimire Chettri <samratsisir294@gmail.com>
2022-01-04 18:20:36 -0800

    Update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    asdf
+    <div class="site-footer">
+      asdf
+    </div>
   </div>
 </template>
 <script>
@@ -8,3 +10,13 @@ export default {
   name: 'AppFooter'
 }
 </script>
+<style>
+.site-footer {
+  left: 0;
+  right: 0;
+  height: 70px;
+  padding: 1rem .5rem;
+  border-top: 2px solid #efefef;
+  margin-top: 1.5rem;
+}
+</style>
