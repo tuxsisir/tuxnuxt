0d946fbd196558cf4401bc9123800723f82c5abb
Sisir Ghimire Chettri <samratsisir294@gmail.com>
2022-01-04 18:21:01 -0800

    Add footer in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,14 +3,16 @@
     <div class="container mx-auto">
       <app-header />
       <Nuxt />
+      <app-footer />
     </div>
   </div>
 </template>
 <script>
 import AppHeader from '@/components/Header'
+import AppFooter from '@/components/Footer'
 
 export default {
   name: 'DefaultLayout',
-  components: { AppHeader }
+  components: { AppHeader, AppFooter }
 }
 </script>
