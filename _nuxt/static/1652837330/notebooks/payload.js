c4dbc70259b8b9fdf61ed4ad036fd0a81a9c617a
tuxsisir <tuxsisir@users.noreply.github.com>
2022-05-18 01:28:54 +0000

    deploy: 3ef885e94d3bb08f64ea5e3357730230946ddb79

---
+++ _nuxt/static/1652837330/notebooks/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/notebooks", {data:[{},{allArticles:[{slug:"cookbook",title:"Cookbook",date:"2020-05-25T00:00:00.000Z",tags:["python"]}],countedTags:{python:1}}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1652837330/tags/stats/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/tags/stats", (function(a,b,c,d){return {data:[{filteredPosts:[{slug:"business-stats",title:"Stats",tags:["stats"]}],countedTags:{agile:a,python:d,database:b,workflow:a,productivity:a,devops:b,mysql:a,cheatsheet:b,postgresql:a,stats:a,preps:a,other:a,containerization:a,docker:c,git:d,pypi:a,markdown:a,excel:a,vim:c,vue:c}}],fetch:{},mutations:void 0}}(1,4,2,3)));
