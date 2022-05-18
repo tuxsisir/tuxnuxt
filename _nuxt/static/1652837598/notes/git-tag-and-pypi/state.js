5ce467165f9c1b7c5e24ede6c6286ec48d9b2f6c
tuxsisir <tuxsisir@users.noreply.github.com>
2022-05-18 07:12:29 +0000

    deploy: 65f0d5ebd7abc084e98442752d50807d0dc7dfca

--- _nuxt/static/1652837598/notes/git-tag-and-pypi/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652837598",layout:"default",error:a,serverRendered:b,routePath:"\u002Fnotes\u002Fgit-tag-and-pypi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"33303463"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
