015e5643c76d8251689dd807c82bd012fd7494d9
tuxsisir <tuxsisir@users.noreply.github.com>
2022-05-19 06:55:21 +0000

    deploy: 6a67adf49d6bdda953d8fc42d93d67b3b7d66819

--- _nuxt/static/1652857945/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652857945",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fd9bbacd"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
---
+++ _nuxt/static/1652943317/tags/python/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652943317",layout:"default",error:a,serverRendered:b,routePath:"\u002Ftags\u002Fpython",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b69f18ba"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
