04c6e93f03c529c8d88928b3f6ba6bbfb504f16a
Sisir Ghimire Chettri <samratsisir294@gmail.com>
2022-05-20 18:10:25 -0700

    Push updates

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="border-t-2 border-gray-300 dark:border-gray-700 text-sm text-gray-600 mt-6 py-6 transition ease-linear duration-200">
-      Copyleft SGC <span class="copyleft">&copy;</span> {{ currentYear }} ~
+      Copyright SGC &copy; {{ year }} ~
       Built with <a href="https://nuxtjs.org/" class="hover:underline" target="_blank">NuxtJS</a>,
       <a href="https://tailwindcss.com/" class="hover:underline" target="_blank">Tailwind CSS</a>, <a
         href="https://pages.github.com/"
@@ -16,7 +16,7 @@ export default {
   name: 'AppFooter',
   data () {
     return {
-      currentYear: new Date().getFullYear()
+      year: 2020 // start of the publication
     }
   }
 }
